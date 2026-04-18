73b55d3851ecbd4628b8136ae49020df31d17895
Lakshya Srivastava <lakshyasri1306@gmail.com>
2026-04-18 15:07:36 +0530

    Time: 18 ms (82.01%) | Memory: 17.8 MB (93.08%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,30 +1,18 @@
 class Solution {
 public:
-    int solve(vector<int>& dp,int n,vector<int>& coins,int amount){
-        if(amount==0)
-        return 0;
-
-        if(amount<0)
-        return INT_MAX;
-        if(dp[amount]!=-1)
-        return dp[amount];
-        int mini=INT_MAX;
-        for(int i=0;i<n;i++){
-            int ans=solve(dp,n,coins,amount-coins[i]);
-            if(ans!=INT_MAX)
-            mini=min(mini,1+ans);
-        }
-        dp[amount]=mini;
-        return dp[amount];
-    }
     int coinChange(vector<int>& coins, int amount){
-        int n=coins.size();
-        vector<int>dp(amount+1,-1);
-        int res=solve(dp,n,coins,amount);
-        
-        if(res==INT_MAX) 
-        return -1;
-        else
-        return res;       
+      vector<int> dp(amount+1,INT_MAX);
+      dp[0]=0;
+
+      for(int i=1;i<=amount;i++){
+        for(int j=0;j<coins.size();j++){
+            if(i-coins[j]>=0 && dp[i-coins[j]]!=INT_MAX)
+            dp[i]=min(dp[i],1+dp[i-coins[j]]);
+        }
+      }   
+      if(dp[amount]!=INT_MAX)
+      return dp[amount];
+      else
+      return -1;
     }
 };
