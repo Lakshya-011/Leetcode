fbe306b0304e9e274f51880ae1558f630d94a508
Lakshya Srivastava <lakshyasri1306@gmail.com>
2026-07-10 09:03:25 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (61.05%) - LeetSync

--- 1520-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/number-of-steps-to-reduce-a-number-in-binary-representation-to-one.cpp
+++ 1520-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/number-of-steps-to-reduce-a-number-in-binary-representation-to-one.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     int numSteps(string s) {
-        int ans=0;
-        int j=0;
-        for(int i=s.length()-1; i>0;i--){
-            int bit =(s[i]-'0') + j;
+        int cnt=0;
+        int c=0;
+        for(int i=s.length()-1;i>0;i--){
+            int b=(s[i]-'0')+c;
 
-            if(bit==1){
-                ans+=2;
-                j=1;
+            if(b==1){
+                cnt+=2;
+                c=1;
             }
             else
-            ans+=1;
+            cnt+=1;
         }
-        return ans+j;
+        return cnt+c;
     }
 };
