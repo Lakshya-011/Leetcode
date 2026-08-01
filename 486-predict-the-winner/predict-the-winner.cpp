8db9e59472e0bb1caba52d87fd7b6628fcc0b71a
Lakshya Srivastava <lakshyasri1306@gmail.com>
2026-08-01 11:56:49 +0530

    Time: 143 ms (8.29%) | Memory: 9.7 MB (69.71%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.cpp
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -1,22 +1,12 @@
 class Solution {
-
-    private:
-    int diff(vector<int>& nums,int l,int r,vector<vector<int>>& dp){
-        if(l==r)
-        return nums[l];
-        if(dp[l][r]!=-1)
-        return dp[l][r];
-        int pickleft= nums[l]-diff(nums,l+1,r,dp);
-        int pickright=nums[r]-diff(nums,l,r-1,dp);
-
-        dp[l][r] = max(pickleft,pickright);
-        return dp[l][r];
-    }
 public:
+int solve(int i,int j,vector<int> &nums){
+    if(i>j)
+    return 0;
+    return max(nums[i]-solve(i+1,j,nums),nums[j]-solve(i,j-1,nums));
+}
     bool predictTheWinner(vector<int>& nums) {
-    int n=nums.size();
-    vector<vector<int>> dp(n, vector<int>(n, -1));
-    int ans=diff(nums,0,n-1,dp);
-    return ans>=0;
+        int n=nums.size();
+        return solve(0,n-1,nums)>=0;
     }
 };
