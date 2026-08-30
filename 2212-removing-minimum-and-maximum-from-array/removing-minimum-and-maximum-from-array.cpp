62c3dbd2d6881220a918c1dea7af7cf3645454c2
Lakshya Srivastava <lakshyasri1306@gmail.com>
2026-08-30 12:11:20 +0530

    Time: 4 ms (24.32%) | Memory: 92 MB (58.82%) - LeetSync

--- 2212-removing-minimum-and-maximum-from-array/removing-minimum-and-maximum-from-array.cpp
+++ 2212-removing-minimum-and-maximum-from-array/removing-minimum-and-maximum-from-array.cpp
@@ -1,20 +1,24 @@
 class Solution {
 public:
     int minimumDeletions(vector<int>& nums) {
-        if(nums.size()==1)
-        return 1;
+        if(nums.size()<=2)
+        return nums.size();
+
+        int idx1,idx2;
+        int mx=*max_element(nums.begin(),nums.end());
+        int mi=*min_element(nums.begin(),nums.end());
+
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==mx)
+            idx1=i;
+
+            if(nums[i]==mi)
+            idx2=i;
+        }
         int n=nums.size();
-        int x = *max_element(nums.begin(), nums.end());
-        int y = *min_element(nums.begin(), nums.end());
+        if(idx2>idx1)
+        swap(idx2,idx1);
 
-        int maxi = find(nums.begin(), nums.end(), x) - nums.begin();
-        int mini = find(nums.begin(), nums.end(), y) - nums.begin();
-
-        int c1 = max(maxi, mini) + 1;
-        int c2 = n - min(maxi, mini);
-        int c3 = (min(maxi, mini) + 1) + (n - max(maxi, mini));
-
-        return min({c1,c2,c3});
-
+        return min(((n-idx1)+min(idx2+1,idx1-idx2)),((idx2+1)+min(n-idx1,idx1-idx2)));
     }
 };
